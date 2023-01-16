586d4248fa2d311b69219ef389936e55286ecffc
Stephen Jones <stephen-jones@hotmail.co.uk>
2023-01-16 11:40:10 +0000

    controller and model created for get topics

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { getTreasures } = require("./controllers/app.controllers");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/topics", );
+
+module.exports = app;
---
+++ controllers/app.controllers.js
@@ -0,0 +1,9 @@
+
+exports.getTopics = (req, res) => {
+
+    selectTopics()
+    .then((topics) => {
+        res.status(200).send({topics})
+    })
+
+}
