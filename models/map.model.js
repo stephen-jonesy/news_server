a3d3568ba2ed505429a4324e2807780a7981b97c
Stephen Jones <stephen-jones@hotmail.co.uk>
2023-01-20 08:51:14 +0000

    utf8 added to readfile argument

--- models/map.model.js
+++ models/map.model.js
@@ -2,7 +2,7 @@ const db = require("../db/connection");
 const fs = require("fs/promises");
 
 exports.selectJSON = () => {
-    return fs.readFile('endpoints.json')
+    return fs.readFile('endpoints.json', 'utf8')
     .then((data) => {
         // Do something with the data]
         parsedData = JSON.parse(data)
